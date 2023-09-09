d998f677f94cbb054d407da93d47c15abd0760f3
Tadeo Deluca <tadeo.deluca2002@gmail.com>
2023-09-09 19:15:56 -0300

    Remove database-related components

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-from django.db.models.fields import CharField, URLField
-from django.db.models.fields.files import ImageField
-
-class Project(models.Model):
-    title = CharField(max_length=150)
-    description = CharField(max_length=500)
-    image = ImageField(upload_to='portfolio/images', blank=True)
-    url = URLField(blank=True)
-    year = models.IntegerField(blank=True, null=True) 
-    tags = CharField(max_length=100, blank=True)
-    featured = models.BooleanField(default=False, blank=True)
--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 import sys
 
-sys.path.append("/Users/tadeodeluca/Documents/django-portfolio/portfolio")
-from portfolio.models import Project
+#sys.path.append("/Users/tadeodeluca/Documents/django-portfolio/portfolio")
+#from portfolio.models import Project
 
 # Create your views here.
 def render_projects(request):
-    projects = Project.objects.all()
-
-    return render(request, 'archive.html', {'projects': projects})
+    return render(request, 'archive.html')
