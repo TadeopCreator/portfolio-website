df52ede0533ee0ccf9d0866f3489dfccdaf8bf72
Tadeo Deluca <tadeo.deluca2002@gmail.com>
2023-09-10 23:49:07 -0300

    sqlite3 database updated

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -134,22 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / "static"
-    ]
-else:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = BASE_DIR / 'media'
-
 MEDIA_URL = '/media/'
 
 # Default primary key field type
