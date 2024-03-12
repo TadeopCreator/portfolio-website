2e9b1625a107b11979327e0a852673e2b25e4c44
Tadeo Deluca <tadeo.deluca2002@gmail.com>
2024-03-12 19:48:18 -0300

    Load certificates from Firestore

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,11 +6,16 @@ from firestore_config import db
 def home(request):
     # Get document from projects collection
     docs_ref = db.collection('projects').order_by('rank', direction=firestore.Query.ASCENDING).stream()
-    docs = [doc.to_dict() for doc in docs_ref]
+    docs_projects = [doc.to_dict() for doc in docs_ref]
+
+    # Get documents from certificates collection
+    docs_ref = db.collection('certificates').order_by('rank', direction=firestore.Query.ASCENDING).stream()
+    docs_certs = [doc.to_dict() for doc in docs_ref]
 
     # Pasa los documentos al contexto
     context = {
-        'projects': docs
+        'projects': docs_projects,
+        'certificates': docs_certs
     }
 
     return render(request, 'home.html', context)
