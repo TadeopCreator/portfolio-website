f727518b77108f53fcb6dd80b6c5b1db8a97b9ca
Tadeo Deluca <tadeo.deluca2002@gmail.com>
2025-03-03 12:38:17 -0300

    Improve 404 page design and update Docker configuration

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Copy the Django project code into the container
 COPY . .
 
-#ENV GOOGLE_APPLICATION_CREDENTIALS="./application_default_credentials.json"
+ENV GOOGLE_APPLICATION_CREDENTIALS="./application_default_credentials.json"
 
 # Run the Django application
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -20,11 +20,9 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -33,7 +31,7 @@ DEBUG = False
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 else:
-    ALLOWED_HOSTS = ['tadeop.pythonanywhere.com', '*']
+    ALLOWED_HOSTS = ['*']
 
 # Application definition
 
